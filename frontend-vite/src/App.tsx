f829137f8f6e890c99ad5641bb12c5cbcf52a4ba
Brandy Garland <brandy.e.garland@gmail.com>
2025-07-30 17:01:46 -0600

    move clock to upper left

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -4,14 +4,14 @@ import DigitalClock from "./components/DigitalClock";
 function App() {
   return (
     <div className="bg-[#222222] h-screen w-screen flex flex-col justify-start items-start relative overflow-hidden">
-      <div className="w-screen flex justify-end pt-2 pe-2">
-        <DigitalClock />
+      <div className="w-screen flex justify-start pt-5 pe-5">
+        <div className="flex flex-col justify-center">
+          <AnalogClock />
+          <DigitalClock />
+        </div>
       </div>
-      <div className="w-screen">
-        <h1 className="text-9xl text-white">Good Morning Charlie</h1>
-      </div>
-      <div className="absolute bottom-5 right-5">
-        <AnalogClock />
+      <div className="w-screen h-[80vw] flex flex-col justify-end items-center pb-10">
+        <h1 className="text-7xl text-white">Good Morning Charlie</h1>
       </div>
     </div>
   );
--- frontend-vite/src/components/DigitalClock.tsx
+++ frontend-vite/src/components/DigitalClock.tsx
@@ -3,7 +3,9 @@ import useClock from "use-clock";
 const DigitalClock = () => {
   const { time } = useClock("hh:mm:ss");
 
-  return <p className="text-sm font-bold text-white">{time}</p>;
+  return (
+    <p className="text-sm font-bold text-white text-center mt-2">{time}</p>
+  );
 };
 
 export default DigitalClock;
