0b13f88cc7493c393eaeeb4ebbd0f153ee9dd051
Brandy Garland <brandy.e.garland@gmail.com>
2025-07-30 15:46:38 -0600

    change to black bg

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,12 +1,7 @@
 function App() {
   return (
-    <div className="bg-dashg-green h-screen w-screen flex justify-center items-start p-20">
-
-
-
-      <h1 className="text-9xl text-white">
-        Good Morning Charlie
-      </h1>
+    <div className="bg-[#222222] h-screen w-screen flex justify-center items-start p-20">
+      <h1 className="text-9xl text-white">Good Morning Charlie</h1>
     </div>
   );
 }
