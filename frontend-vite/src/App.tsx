dbfa2039d37a7ad1f1936cf1ebf052761c301807
Brandy Garland <brandy.e.garland@gmail.com>
2025-07-31 00:09:28 -0600

    add a screensaver

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -3,6 +3,7 @@ import AnalogClock from "./components/AnalogClock";
 import DigitalClock from "./components/DigitalClock";
 import GreetingMessage from "./components/GreetingMessage";
 import HourCalendar from "./components/HourCalendar";
+import ScreenSaver from "./components/ScreenSaver";
 
 export const ThemeContext = createContext({
   primaryColor: "bg-green-600",
@@ -11,20 +12,22 @@ export const ThemeContext = createContext({
 function App() {
   return (
     <ThemeContext.Provider value={{ primaryColor: "bg-orange-600" }}>
-      <div className="bg-black h-screen w-screen flex flex-col justify-start items-start relative overflow-hidden">
-        <div className="w-screen flex justify-start pt-5 ps-5">
-          <div className="flex flex-col justify-center">
-            <AnalogClock />
-            <DigitalClock />
+      <ScreenSaver>
+        <div className="bg-black h-screen w-screen flex flex-col justify-start items-start relative overflow-hidden">
+          <div className="w-screen flex justify-start pt-5 ps-5">
+            <div className="flex flex-col justify-center">
+              <AnalogClock />
+              <DigitalClock />
+            </div>
+          </div>
+          <div className="w-screen h-[80vw] flex flex-col justify-end items-center pb-10 theme-gradient">
+            <h1 className="text-7xl text-white">
+              <GreetingMessage />
+            </h1>
+            <HourCalendar />
           </div>
         </div>
-        <div className="w-screen h-[80vw] flex flex-col justify-end items-center pb-10">
-          <h1 className="text-7xl text-white">
-            <GreetingMessage />
-          </h1>
-          <HourCalendar />
-        </div>
-      </div>
+      </ScreenSaver>
     </ThemeContext.Provider>
   );
 }
---
+++ frontend-vite/src/components/ScreenSaver.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const ScreenSaver: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [hr, setHr] = useState(new Date().getHours());
+
+  const wakeUp = 6;
+  const sleep = 9;
+
+  const saveScreen = hr <= wakeUp || hr >= sleep;
+
+  useEffect(() => {
+    // Setup the interval when the component mounts
+    const intervalId = setInterval(() => {
+      setHr(new Date().getHours());
+    }, 60000);
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures effect runs only once on mount
+
+  return (
+    <>
+      {saveScreen ? <div className="bg-black h-screen w-screen" /> : children}
+    </>
+  );
+};
+
+export default ScreenSaver;
