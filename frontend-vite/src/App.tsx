6d2d3d8b447c24d8f5e8c9940bd935d4c6efb257
Brandy Garland <brandy.e.garland@gmail.com>
2025-07-30 23:44:14 -0600

    scaffold hour calendar

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -1,24 +1,31 @@
+import { createContext } from "react";
 import AnalogClock from "./components/AnalogClock";
 import DigitalClock from "./components/DigitalClock";
 import GreetingMessage from "./components/GreetingMessage";
 import HourCalendar from "./components/HourCalendar";
 
+export const ThemeContext = createContext({
+  primaryColor: "bg-green-600",
+});
+
 function App() {
   return (
-    <div className="bg-black h-screen w-screen flex flex-col justify-start items-start relative overflow-hidden">
-      <div className="w-screen flex justify-start pt-5 ps-5">
-        <div className="flex flex-col justify-center">
-          <AnalogClock />
-          <DigitalClock />
+    <ThemeContext.Provider value={{ primaryColor: "bg-orange-600" }}>
+      <div className="bg-black h-screen w-screen flex flex-col justify-start items-start relative overflow-hidden">
+        <div className="w-screen flex justify-start pt-5 ps-5">
+          <div className="flex flex-col justify-center">
+            <AnalogClock />
+            <DigitalClock />
+          </div>
+        </div>
+        <div className="w-screen h-[80vw] flex flex-col justify-end items-center pb-10">
+          <h1 className="text-7xl text-white">
+            <GreetingMessage />
+          </h1>
+          <HourCalendar />
         </div>
       </div>
-      <div className="w-screen h-[80vw] flex flex-col justify-end items-center pb-10">
-        <h1 className="text-7xl text-white">
-          <GreetingMessage />
-        </h1>
-        <HourCalendar />
-      </div>
-    </div>
+    </ThemeContext.Provider>
   );
 }
 
--- frontend-vite/src/components/HourCalendar.tsx
+++ frontend-vite/src/components/HourCalendar.tsx
@@ -1,17 +1,64 @@
-import TimeLine from "react-daily-timeline-sheet";
+import { useContext, useEffect, useState } from "react";
+import { ThemeContext } from "../App";
 
 const HourCalendar = () => {
-  const workingHours = [
-    {
-      start: "07:00",
-      end: "20:00",
-      title: "Awake Time",
-    },
-  ];
+  const [hr, setHr] = useState(new Date().getHours());
+  const [min, setMin] = useState(new Date().getMinutes());
+
+  console.log("min", min);
+
+  const hours = Array.from({ length: 23 }, (_, index) => index);
+
+  useEffect(() => {
+    // Setup the interval when the component mounts
+    const intervalId = setInterval(() => {
+      setHr(new Date().getHours());
+      setMin(new Date().getMinutes());
+    }, 10000); // Update every 5 mins
+
+    // Cleanup function to clear the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, []); // Empty dependency array ensures effect runs only once on mount
+
+  const wakeUp = 8;
+  const bedTime = 20;
+
+  const { primaryColor } = useContext(ThemeContext);
 
   return (
-    <div className="w-[80vw]">
-      <TimeLine notWorkingCaption="" workingHours={workingHours} height="20px" nonWorkingColor="#222222" workingColor="orange"/>
+    <div className="w-[1440px] text-white rounded-lg flex flex-row justify-center py-4">
+      {hours.map((hour) => {
+        return (
+          <>
+            <div
+              className={`w-20 border flex justify-center ${
+                hour >= wakeUp && hour < bedTime ? primaryColor : ""
+              }`}
+            >
+              {hour}
+            </div>
+          </>
+        );
+      })}
+      <div
+        className="absolute text-white bottom-0 h-[50px]"
+        style={{ left: `${(hr * 60) + (min)}px` }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-8"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="m4.5 15.75 7.5-7.5 7.5 7.5"
+          />
+        </svg>
+      </div>
     </div>
   );
 };
