4dc9c39a45668edbcfa4873a550e9626e71c824d
Brandy Garland <brandy.e.garland@gmail.com>
2025-07-31 23:39:40 -0600

    co log unused vars for now

--- frontend-vite/src/components/ScreenSaver.tsx
+++ frontend-vite/src/components/ScreenSaver.tsx
@@ -6,9 +6,10 @@ const ScreenSaver: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const wakeUp = 6;
   const sleep = 9;
 
+  console.log(hr, wakeUp, sleep);
   // const saveScreen = hr <= wakeUp || hr >= sleep;
   const saveScreen = false;
-  
+
   useEffect(() => {
     // Setup the interval when the component mounts
     const intervalId = setInterval(() => {
