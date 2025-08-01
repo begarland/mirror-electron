30e3a9be3622057719e72d2e002562c569c19fc7
Brandy Garland <brandy.e.garland@gmail.com>
2025-08-01 16:43:38 -0600

    resize right side

--- frontend-vite/src/App.tsx
+++ frontend-vite/src/App.tsx
@@ -15,13 +15,13 @@ function App() {
     <ThemeContext.Provider value={{ primaryColor: "bg-green-600" }}>
       <ScreenSaver>
         <div className="bg-black h-screen w-screen flex justify-start items-start relative overflow-hidden">
-          <div className="w-4/5 flex justify-center items-end h-screen mb-36">
+          <div className="w-4/6 flex justify-center items-end h-screen mb-36">
             <h1 className="text-7xl text-white font-dailycroquete">
               <GreetingMessage />
             </h1>
           </div>
 
-          <div className="w-1/5 flex justify-end">
+          <div className="w-1/3 flex justify-end">
             <div className="flex flex-col">
               <TodayIs />
               <div className="flex flex-col justify-center border-y-4 p-4 border-t-[#d72638] border-b-[#f46036] ">
